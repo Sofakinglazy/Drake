e487eb736efe2016aa5b39c4427c326482efe74d
Sofakinglazy <yx5m14@soton.ac.uk>
2016-04-19 22:59:17 +0100

    Add door script

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	void OnTriggerEnter(Collider2D other){
+		if (other.gameObject.tag == "Player"){
+//			&& Input.GetButtonDown(KeyCode.UpArrow)) {
+			DontDestroyOnLoad (other.gameObject);
+			Application.LoadLevel("Scene1");
+		}
+
+	}
+}
