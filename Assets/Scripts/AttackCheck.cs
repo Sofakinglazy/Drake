59b27319a9cc38a1a77a340ebfabbcb2779b2f56
Yaowjian <yaowjian@yaowjiandeMacBook-Pro.local>
2016-04-20 16:01:22 +0100

    knock back

--- Assets/Scripts/AttackCheck.cs
+++ Assets/Scripts/AttackCheck.cs
@@ -4,22 +4,41 @@ using System.Collections;
 public class AttackCheck : MonoBehaviour {
 
 	private Player player;
+	private SimpleEnemy Knight;
+	private float v;
 	void Start(){
 		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<Player> ();
+		Knight =  GameObject.FindGameObjectWithTag ("Knight").GetComponent<SimpleEnemy> ();
+		v = Knight.velocity;
 	}
 
-	void OnTriggerEnter2D(Collider2D col){
+//	void OnTriggerEnter2D(Collider2D col){
+//		if (col.CompareTag ("Player")) {
+//			player.Damage (20);
+//		}
+//	}
+
+	void OnTriggerStay2D(Collider2D col){
 		if (col.CompareTag ("Player")) {
-			player.Damage (20);
-		}
+				player.Damage (1);
+			Knight.velocity = 0;
+			Knight.anim.SetBool ("Attack", true);
+//			StartCoroutine (player.Knockback (0.05f,500,player.transform.position));
+			player.knockbackCount=player.knockbackLength;
+			if (player.transform.position.x < transform.position.x) {
+				player.knockfromRight = true;
+			} else {
+				player.knockfromRight = false;
+			}
+
+				
+
+			}
 	}
 
-//	void OnTriggerStay2D(Collider2D col){
-//		player.curHealth -= 50;
-//	}
-
-//	void OnTriggerExit2D(Collider2D col){
-//		player.grounded = false;
-//	}
+	void OnTriggerExit2D(Collider2D col){
+		Knight.velocity = v;
+		Knight.anim.SetBool ("Attack", false);
+	}
 
 }
